977bc0a8fbc70e42509628a4433a3eb93cfef1df
Sebastian Sędzik <s.sedzik@smartrecruiters.com>
2024-01-01 17:42:51 +0100

    fix: rewrite error messages to be more detailed

--- lib/errors.ts
+++ lib/errors.ts
@@ -1,17 +1,37 @@
 export class NotSuiteDecoratedMethodError extends Error {
   constructor(decoratorName: string ,method: any) {
-    super(`${decoratorName} decorator can be applied only to methods decorated by @suite. ${method?.name} is not decorated by @suite`);
+    super(`
+The @${decoratorName} decorator can only be used on class that also have the @suite decorator.
+Make sure ${method?.name} is marked with @suite, and that ${decoratorName} comes before @suite, like this:
+
+@${decoratorName}
+@suite()
+${method?.name}() {}`);
   }
 }
 
 export class NotTestDecoratedMethodError extends Error {
   constructor(decoratorName: string, method: any) {
-    super(`${decoratorName} decorator can be applied only to methods decorated by @test. ${method?.name} is not decorated by @test`);
+    super(`
+The @${decoratorName} decorator can only be used on methods that also have the @test decorator.
+Make sure ${method?.name} is marked with @test, and that ${decoratorName} comes before @test, like this:
+
+@${decoratorName}
+@test()
+${method?.name}() {}`
+    );
   }
 }
 
 export class NotSuiteOrTestDecoratedMethodError extends Error {
   constructor(decoratorName: string, method: any) {
-    super(`${decoratorName} decorator can be applied only to methods decorated by @test or classes decorated by @suite. ${method?.name} is not decorated by any of them`);
+    super(`
+The @${decoratorName} decorator can only be used on classes/methods that also have the @suite or @test decorator.
+Make sure ${method?.name} is marked with @suite or @test, and that ${decoratorName} comes before @suite or @test, like this:
+
+@${decoratorName}
+@suite() / @test()
+${method?.name}() {}
+    `);
   }
 }
